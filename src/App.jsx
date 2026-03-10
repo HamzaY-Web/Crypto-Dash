ca7d355988ff9131b07088ee8b8a2c8b524edaa7
Hamza <hamzayousefcs@gmail.com>
2026-03-10 12:11:10 +0300

    fetching data from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,31 @@
-
+import { useState, useEffect } from "react"
+const API_URL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
 
 function App() {
+
+  const [coins, setCoins] = useState([]) 
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+  useEffect(()=>{
+
+    const fetchCoins = async () => {
+
+      try {
+        const res = await fetch(API_URL)
+        if(!res.ok) {throw new Error('Failed to fetch data')}
+        const data = res.json()
+        console.log(data);
+        setCoins(data)
+      } catch (err) {
+        setError(err.message)
+      } finally{
+      setLoading(false)
+      }
+    }
+
+    fetchCoins();
+  }, []);
 
   return (
     <>

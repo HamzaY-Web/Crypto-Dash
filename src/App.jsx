cbe68c3a92c602651d675c427c97c16d7dc03613
Hamza <hamzayousefcs@gmail.com>
2026-03-10 13:56:25 +0300

    Adding limit of cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import { useState, useEffect } from "react"
 import CoinCard from "./components/CoinCard"
+import LimitSelector from "./components/LimitSelector"
 
-const API_URL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+const API_URL = import.meta.env.VITE_API_URL
 
 function App() {
 
   const [coins, setCoins] = useState([])
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
+  const [limit, setLimit] = useState(10)
 
   useEffect(() => {
 
     const fetchCoins = async () => {
 
       try {
-        const res = await fetch(API_URL)
+        const res = await fetch(`${API_URL}&order=market_cap_desc&per_page=${limit}&page=1&sparkline=false`)
         if (!res.ok) { throw new Error('Failed to fetch data') }
         const data = await res.json()
         console.log(data);
@@ -27,13 +29,15 @@ function App() {
     }
 
     fetchCoins();
-  }, []);
+  }, [limit]);
 
   return (
     <>
       <h1>🚀 Crypto Dash</h1>
       {loading && <p>Loading...</p>}
       {error && <div className="error">{error}</div>}
+
+      <LimitSelector limit={limit} onLimitChange={setLimit}/>
 
       {!loading && !error && (
         <main className="grid">
